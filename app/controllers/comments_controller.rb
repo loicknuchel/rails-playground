88643366243ffb9006a3c96862721796aa34370e
Loïc Knuchel <loic.knuchel@doctolib.com>
2021-02-04 10:17:48 +0100

    Add basic authentication

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,7 @@
 class CommentsController < ApplicationController
+
+  http_basic_authenticate_with name: 'admin', password: 'admin', only: %i[destroy]
+
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
